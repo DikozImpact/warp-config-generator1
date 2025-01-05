b0d21fd926082baf68a356e7f46d94dcaf61da2b
DikozImpact <22dikoz22@gmail.com>
2025-01-05 17:34:17 +0300

    Update script.js

--- public/static/script.js
+++ public/static/script.js
@@ -70,9 +70,119 @@ async function generateConfig2() {
     }
 }
 
-document.getElementById('generateButton2').onclick = generateConfig2;
+async function generateConfig3() {
+    const button = document.getElementById('generateButton3');
+    const button_text = document.querySelector('#generateButton3 .button__text');
+    const status = document.getElementById('status');
+    const randomNumber = Math.floor(Math.random() * (999 - 100 + 1)) + 100;
 
-document.getElementById('generateButton3').onclick = generateConfig;
+    // Изменяем состояние кнопки на загрузку
+    button.disabled = true;
+    button.classList.add("button--loading");
+
+    try {
+        const response = await fetch(`/warp`);
+        const data = await response.json();
+
+        if (data.success) {
+            const downloadFile = () => {
+                const link = document.createElement('a');
+                link.href = 'data:text/plain;base64,' + data.content;
+                link.download = `warp_llimonix_${randomNumber}.conf`;
+                link.click();
+            };
+
+            button_text.textContent = `Скачать warp_llimonix_${randomNumber}.conf`;
+            button.onclick = downloadFile;
+            downloadFile();
+        } else {
+            status.textContent = 'Ошибка: ' + data.message;
+        }
+    } catch (error) {
+        status.textContent = 'Произошла ошибка при генерации.';
+    } finally {
+        button.disabled = false;
+        button.classList.remove("button--loading");
+    }
+}
+
+async function generateConfig4() {
+    const button = document.getElementById('generateButton4');
+    const button_text = document.querySelector('#generateButton4 .button__text');
+    const status = document.getElementById('status');
+    const randomNumber = Math.floor(Math.random() * (999 - 100 + 1)) + 100;
+
+    // Изменяем состояние кнопки на загрузку
+    button.disabled = true;
+    button.classList.add("button--loading");
+
+    try {
+        const response = await fetch(`/warp`);
+        const data = await response.json();
+
+        if (data.success) {
+            const downloadFile = () => {
+                const link = document.createElement('a');
+                link.href = 'data:text/plain;base64,' + data.content;
+                link.download = `warp_llimonix_${randomNumber}.conf`;
+                link.click();
+            };
+
+            button_text.textContent = `Скачать warp_llimonix_${randomNumber}.conf`;
+            button.onclick = downloadFile;
+            downloadFile();
+        } else {
+            status.textContent = 'Ошибка: ' + data.message;
+        }
+    } catch (error) {
+        status.textContent = 'Произошла ошибка при генерации.';
+    } finally {
+        button.disabled = false;
+        button.classList.remove("button--loading");
+    }
+}
+
+async function generateConfig5() {
+    const button = document.getElementById('generateButton5');
+    const button_text = document.querySelector('#generateButton5 .button__text');
+    const status = document.getElementById('status');
+    const randomNumber = Math.floor(Math.random() * (999 - 100 + 1)) + 100;
+
+    // Изменяем состояние кнопки на загрузку
+    button.disabled = true;
+    button.classList.add("button--loading");
+
+    try {
+        const response = await fetch(`/warp`);
+        const data = await response.json();
+
+        if (data.success) {
+            const downloadFile = () => {
+                const link = document.createElement('a');
+                link.href = 'data:text/plain;base64,' + data.content;
+                link.download = `warp_llimonix_${randomNumber}.conf`;
+                link.click();
+            };
+
+            button_text.textContent = `Скачать warp_llimonix_${randomNumber}.conf`;
+            button.onclick = downloadFile;
+            downloadFile();
+        } else {
+            status.textContent = 'Ошибка: ' + data.message;
+        }
+    } catch (error) {
+        status.textContent = 'Произошла ошибка при генерации.';
+    } finally {
+        button.disabled = false;
+        button.classList.remove("button--loading");
+    }
+}
+
+document.getElementById('generateButton2').onclick = generateConfig2;
+document.getElementById('generateButton3').onclick = generateConfig3;
+document.getElementById('generateButton4').onclick = generateConfig4;
+document.getElementById('generateButton5').onclick = generateConfig5;
+
 
 document.getElementById('generateButton').onclick = generateConfig;
 
