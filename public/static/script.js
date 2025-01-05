0c2759594c2633302de527309aadfea464f2c416
DikozImpact <22dikoz22@gmail.com>
2025-01-05 17:04:05 +0300

    Update script.js

--- public/static/script.js
+++ public/static/script.js
@@ -34,6 +34,43 @@ async function generateConfig() {
     }
 }
 
+async function generateConfig() {
+    const button = document.getElementById('generateButton2');
+    const button_text = document.querySelector('#generateButton2 .button__text');
+    const status = document.getElementById('status');
+    const randomNumber = Math.floor(Math.random() * (999 - 100 + 1)) + 100;
+
+    // Изменяем состояние кнопки на загрузку
+    button.disabled = true;
+    button.classList.add("button--loading");
+
+    try {
+        const response = await fetch2(`/warp`);
+        const data = await response.json();
+
+        if (data.success) {
+            const downloadFile = () => {
+                const link = document.createElement('a');
+                link.href = 'data:text/plain;base64,' + data.content;
+                link.download = `warp_llimonix_${randomNumber}.conf`;
+                link.click();
+            };
+
+            button_text.textContent = `Скачать warp_llimonix_${randomNumber}.conf`;
+            button.onclick = downloadFile;
+            downloadFile();
+        } else {
+            status.textContent = 'Ошибка: ' + data.message;
+        }
+    } catch (error) {
+        status.textContent = 'Произошла ошибка при генерации.';
+    } finally {
+        button.disabled = false;
+        button.classList.remove("button--loading");
+    }
+}
+
+
 document.getElementById('generateButton').onclick = generateConfig;
 
 document.getElementById('telegramButton').onclick = function() {
