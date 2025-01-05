dd9228b0e335cda977f7cfa2a6a4dadbcfa2d7dd
DikozImpact <22dikoz22@gmail.com>
2025-01-05 19:19:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getWarpConfigLink } = require('./warpConfig');
+const { getWarpConfigLink } = require('./warpConfig2');
 const path = require('path');
 
 const app = express();
