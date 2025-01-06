40bccd122b665546d6d11d6a871b3c115dd71ef7
DikozImpact <22dikoz22@gmail.com>
2025-01-07 00:19:19 +0300

    Update warpConfig3.js

--- warpConfig3.js
+++ warpConfig3.js
@@ -107,18 +107,7 @@ const { wprivKey, wpubKey } = wgenerateKeys();
         locale: "en_US"
     };
 
-    const wregResponse = await wapiRequest('POST', 'reg', wregBody);
-    const wid = wregResponse.result.id;
-    const wtoken = wregResponse.result.token;
-    // Включение WARP
-    const wwarpResponse = await wapiRequest('PATCH', `reg/${wid}`, { warp_enabled: true }, wtoken);
-    const wpeer_pub = wwarpResponse.result.config.peers[0].public_key;
-    const wclient_ipv4 = wwarpResponse.result.config.interface.addresses.v4;
-    const wclient_ipv6 = wwarpResponse.result.config.interface.addresses.v6;
-    const wreserved64 = wwarpResponse.result.config.client_id;
-    const wreservedHex = wBuffer.from(wreserved64, 'base64').toString('hex');
-    const wreservedDec = wreservedHex.match(/.{1,2}/g).map(hex => parseInt(hex, 16)).join(', ');
-    const wreservedHex2 = '0x' + wreservedHex;
+   
 
     // Формируем конфиг
     const conf = `{
@@ -142,10 +131,10 @@ const { wprivKey, wpubKey } = wgenerateKeys();
    "type": "wireguard",
    "tag": "WARP in WARP",
    "detour": "WARP",
-   "local_address": ["${wclient_ipv4}/24", "${wclient_ipv6}/128"],
-   "private_key": "${wprivKey}",
+   "local_address": ["${client_ipv4}/24", "${client_ipv6}/128"],
+   "private_key": "${privKey}",
    "peer_public_key": "${peer_pub}",
-   "reserved": [${wreservedDec}],
+   "reserved": [${reservedDec}],
    "mtu": 1280,
    "server": "188.114.97.170",
    "server_port": 1018
