e5e7208d9f9496b8f830c125140e77241eaffab0
DikozImpact <22dikoz22@gmail.com>
2025-01-19 02:08:39 +0300

    Update warpConfig3.js

--- warpConfig3.js
+++ warpConfig3.js
@@ -136,7 +136,7 @@ const { wprivKey, wpubKey } = wgenerateKeys();
 "local_address": ["${client_ipv4}/24", "${client_ipv6}/128"],
 "peer_public_key": "${peer_pub}",
 "server": "188.114.97.170",
-"server_port": 2408
+"server_port": 500
 },
   {
    "type": "wireguard",
