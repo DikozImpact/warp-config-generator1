477cb45e701e15b2119e68d24218c5e9e9954edd
DikozImpact <22dikoz22@gmail.com>
2025-03-23 01:28:45 +0300

    Update script.js

--- public/static/script.js
+++ public/static/script.js
@@ -300,38 +300,11 @@ document.getElementById('telegramButton').onclick = function() {
     window.location.href = 'https://t.me/warp_1_1_1_1';
 }
 
-document.getElementById('DonationAlertsButton').onclick = function() {
-    window.location.href = 'https://www.donationalerts.com/r/warphelp';
-}
 
-document.getElementById('BoostyNewButton').onclick = function() {
+document.getElementById('BoostyButton').onclick = function() {
     window.location.href = 'https://boosty.to/warphelp/donate';
 }
 
-document.getElementById('BoostyButton').onclick = function() {
-    const newButtons = document.getElementById('newButtons');
-
-    if (newButtons.classList.contains('show')) {
-        // Если блок видим, скрываем его с анимацией
-        newButtons.classList.remove('show');
-        setTimeout(() => {
-            this.style.display = 'block'; // Показываем кнопку BoostyButton
-        }, 500); // Задержка должна соответствовать длительности анимации
-    } else {
-        // Если блок скрыт, показываем его с анимацией
-        this.style.display = 'none';
-        newButtons.classList.add('show');
-
-        // Добавляем задержку перед прокруткой
-        setTimeout(() => {
-            // Прокручиваем страницу до самого низа
-            window.scrollTo({
-                top: document.body.scrollHeight, // Прокручиваем до конца страницы
-                behavior: 'smooth' // Плавная прокрутка
-            });
-        }, 300); // Увеличиваем задержку до 500 мс
-    }
-};
 
 document.getElementById('githubButton').onclick = function() {
     window.location.href = 'https://docs.google.com/document/d/1DX4X7t7V4QasQJYbps5D1yNtsK7tqsouSMJH2w4AMOY';
